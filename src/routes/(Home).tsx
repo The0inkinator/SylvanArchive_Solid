b2666ad4face46837779ccce6ce05231ecd0adef
TheOinkinator <theoinkinator@gmail.com>
2023-09-24 21:01:56 -0700

    update imports

--- src/routes/(Home).tsx
+++ src/routes/(Home).tsx
@@ -1,6 +1,6 @@
 import styles from "../layouts/HomeLayout.module.css";
-import FrontPageHeader from "~/components/layoutComponents/frontPageHeader/FrontPageHeader";
-import ShelfScene from "~/components/shelfSystem/shelfScene/ShelfScene";
+import FrontPageHeader from "../components/layoutComponents/frontPageHeader/FrontPageHeader";
+import ShelfScene from "../components/shelfSystem/shelfScene/ShelfScene";
 //vercel commit
 
 export default function Home() {
