ed67c9cb5bbb7f7f3b46e479e2432d4671a7a838
TheOinkinator <theoinkinator@gmail.com>
2023-10-05 15:02:54 -0700

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import solid from "solid-start/vite";
 import { defineConfig } from "vite";
+import solid from "solid-start/vite";
 import vercel from "solid-start-vercel";
 
-export default defineConfig(() => {
-  return {
-    plugins: [solid({ ssr: true, adapter: vercel({ edge: true }) })],
-  };
+export default defineConfig({
+  plugins: [solid({ adapter: vercel() })],
 });
