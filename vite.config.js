5182e11c80612c06b2e7fb1fa235ffc39c50b5d9
TheOinkinator <theoinkinator@gmail.com>
2023-10-05 13:20:55 -0700

    test vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
+// import solid from "solid-start/vite";
+// import { defineConfig } from "vite";
+// import vercel from "solid-start-vercel";
+
+// export default defineConfig({
+//   plugins: [solid({ adapter: vercel() })],
+// });
+
+import solidStartVercel from "solid-start-vercel";
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
-import vercel from "solid-start-vercel";
-
 export default defineConfig({
-  plugins: [solid({ adapter: vercel() })],
+  plugins: [
+    solid({
+      adapter: solidStartVercel({ edge: true }),
+    }),
+  ],
 });
