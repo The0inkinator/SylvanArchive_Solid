86c8e7640dd8cf69904beb549b2d94d9cd3af1fc
TheOinkinator <theoinkinator@gmail.com>
2023-10-05 14:03:15 -0700

    remove old config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,3 @@
-// import solid from "solid-start/vite";
-// import { defineConfig } from "vite";
-// import vercel from "solid-start-vercel";
-
-// export default defineConfig({
-//   plugins: [solid({ adapter: vercel() })],
-// });
-
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
 import vercel from "solid-start-vercel";
